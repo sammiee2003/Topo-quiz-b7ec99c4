98d392dce819ceec80d63f14149a77752ce02766
SAM VAN DEN BOSCH <samvandenbosch2003@gmail.com>
2020-03-25 10:53:08 +0100

    lvl 2 opdr 2

--- topo.php
+++ topo.php
@@ -5,15 +5,15 @@ $landen = ["japan" => "tokyo", "mexico" => "mexico city", "usa" => "washington D
 "uk" => "london"];
 
 $j = 0;
-foreach ($landen as $land => $stad){ 
+foreach ($landen as $land => $stad) { 
     echo "Wat is de hoofdstad van " .$land. "?" . PHP_EOL;
     $antwoord = readline("");
-if ($antwoord === $stad){
+    if ($antwoord === $stad) {
     echo "correct!" . PHP_EOL;
     $j++;
-} else{
+    }else{
     echo "Incorrect!" . PHP_EOL; 
-    echo "Het correcte antwoord was:" . " ". $stad . PHP_EOL;
-} 
+    echo "Het correcte antwoord was: ". $stad . PHP_EOL;
+    } 
 }   
 echo $j . " van de 10 goed!";
